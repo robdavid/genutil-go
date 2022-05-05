2821fec7b3702098008d7b9bc160681b7606c0ff
Rob David <rob.j.david@googlemail.com>
2022-05-05 21:52:31 +0100

    Added pipe iterator

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -1,17 +1,25 @@
 package iterator
 
-import "github.com/robdavid/genutil-go/option"
+import (
+	"fmt"
 
+	"github.com/robdavid/genutil-go/option"
+)
+
+// Generic iterator
 type Iterator[T any] interface {
+	// Set the iterator's current value to be the first, and subsequent, iterator elements.
+	// False is returned when there are no more elements (the current value remains unchanged)
 	Next() bool
-	Value() (T, error)
-	MustValue() T
+	Value() (T, error) // Get the current iterator value, or an error if the last call to Next() has failed
+	MustValue() T      // Get the current iterator value. Panics if the last call to Next() has failed.
 }
 
+// Wraps an iterator and adds a mapping function
 type Func[T, U any] struct {
 	base    Iterator[T]
 	mapping func(T) U
-	value   option.OptionRef[U]
+	value   option.Option[U]
 }
 
 func (i *Func[T, U]) Next() bool {
@@ -70,10 +78,13 @@ func Slice[T any](slice []T) Iterator[T] {
 	return &SliceIter[T]{slice, 0, t}
 }
 
+// Wraps an iterator with a mapping function, producing a new iterator
 func Map[T any, U any](iter Iterator[T], mapping func(T) U) Iterator[U] {
-	return &Func[T, U]{iter, mapping, option.Ref[U](nil)}
+	return &Func[T, U]{iter, mapping, option.Empty[U]()}
 }
 
+// Collects all elements from an iterator into a slice. If the iterator
+// returns an error, this call will panic.
 func MustCollect[T any](iter Iterator[T]) []T {
 	if result, err := Collect(iter); err != nil {
 		panic(err)
@@ -82,6 +93,8 @@ func MustCollect[T any](iter Iterator[T]) []T {
 	}
 }
 
+// Collects all elements from an iterator into a slice. If the iterator
+// returns an error, this call will terminate and return that error.
 func Collect[T any](iter Iterator[T]) ([]T, error) {
 	result := make([]T, 0)
 	for iter.Next() {
@@ -92,4 +105,99 @@ func Collect[T any](iter Iterator[T]) ([]T, error) {
 		}
 	}
 	return result, nil
+}
+
+// Contains a value of type T, or an error
+type Result[T any] struct {
+	value T
+	err   error
+}
+
+// Creates a new non-error result
+func NewResult[T any](t T) Result[T] {
+	return Result[T]{t, nil}
+}
+
+// Creates an error result
+func NewError[T any](err error) Result[T] {
+	var t T
+	return Result[T]{t, err}
+}
+
+// An iterator that obtains values (or an error) from
+// a channel
+type PipeIter[T any] struct {
+	source <-chan Result[T]
+	value  Result[T]
+}
+
+func (pi *PipeIter[T]) Next() bool {
+	var ok bool
+	pi.value, ok = <-pi.source
+	return ok
+}
+
+func (pi *PipeIter[T]) Value() (T, error) {
+	return pi.value.value, pi.value.err
+}
+
+func (pi *PipeIter[T]) MustValue() T {
+	if err := pi.value.err; err != nil {
+		panic(err)
+	}
+	return pi.value.value
+}
+
+// An object to which consecutive iterator values are supplied via the Yield
+// method (or an error via the Error method), sent via a channel
+type Yield[T any] struct {
+	sink chan<- Result[T]
+}
+
+func (y Yield[T]) Yield(t T) {
+	y.sink <- NewResult(t)
+}
+
+func (y Yield[T]) Error(err error) {
+	y.sink <- NewError[T](err)
+}
+
+// An error type indicating that a Pipe iterator function has panicked
+type PipePanic struct {
+	panic interface{}
+}
+
+func (pp PipePanic) Error() string {
+	return fmt.Sprintf("panic during pipe iterator: %#v", pp.panic)
+}
+
+// A type alias for a function taking a Yield object and returning
+// an error.
+type PipeFunc[T any] func(Yield[T]) error
+
+func runPipe[T any](y Yield[T], activity PipeFunc[T]) {
+	defer close(y.sink)
+	defer func() {
+		if p := recover(); p != nil {
+			y.Error(PipePanic{p})
+		}
+	}()
+	if err := activity(y); err != nil {
+		y.Error(err)
+	}
+}
+
+// Create an iterator from a function that yields a sequence of results
+// by making calls to Yield(), or Error(), on an object that will be passed to the function.
+// The function is run in a separate goroutine, and its yielded results are sent over a channel
+// to the iterator where they can be consumed in the normal way by calls to Next() and Value().
+// Any error result yielded, or returned by the function, cause that error to be returned by Value()
+// in the iterator.
+//
+// PipeFunc[T] is an alias for func(Yield[T]) error
+func Pipe[T any](activity PipeFunc[T]) Iterator[T] {
+	ch := make(chan Result[T], 1)
+	yield := Yield[T]{ch}
+	go runPipe(yield, activity)
+	return &PipeIter[T]{source: ch}
 }
--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -1,6 +1,7 @@
 package iterator
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +19,70 @@ func TestMapIter(t *testing.T) {
 	expected := []int{2, 4, 6, 8}
 	actual := MustCollect(Map(Slice(input), func(n int) int { return n * 2 }))
 	assert.Equal(t, expected, actual)
+}
+
+func TestPipeIter(t *testing.T) {
+	pipe := Pipe(func(y Yield[int]) error {
+		for i := 0; i < 10; i++ {
+			y.Yield(i)
+		}
+		return nil
+	})
+	actual := MustCollect(pipe)
+	expected := make([]int, 10)
+	for i := range expected {
+		expected[i] = i
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestPipeIterMap(t *testing.T) {
+	pipe := Pipe(func(y Yield[int]) error {
+		for i := 0; i < 10; i++ {
+			y.Yield(i)
+		}
+		return nil
+	})
+	actual := MustCollect(Map(pipe, func(x int) int { return x * 3 }))
+	expected := make([]int, 10)
+	for i := range expected {
+		expected[i] = i * 3
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestPipeIterError(t *testing.T) {
+	pipe := Pipe(func(y Yield[int]) error {
+		for i := 0; i < 10; i++ {
+			y.Yield(i)
+		}
+		return fmt.Errorf("iterator failed")
+	})
+	actual, err := Collect(pipe)
+	expected := make([]int, 10)
+	for i := range expected {
+		expected[i] = i
+	}
+	assert.Equal(t, expected, actual)
+	if assert.Error(t, err) {
+		assert.Equal(t, "iterator failed", err.Error())
+	}
+}
+
+func TestPipeIterPanic(t *testing.T) {
+	pipe := Pipe(func(y Yield[int]) error {
+		for i := 0; i < 10; i++ {
+			y.Yield(i)
+		}
+		panic("iterator failed")
+	})
+	actual, err := Collect(pipe)
+	expected := make([]int, 10)
+	for i := range expected {
+		expected[i] = i
+	}
+	assert.Equal(t, expected, actual)
+	if assert.Error(t, err) {
+		assert.Equal(t, PipePanic{"iterator failed"}, err)
+	}
 }
