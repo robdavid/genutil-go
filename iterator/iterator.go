5d173e47abac5be4160505cb639e8ace7e9a6e45
Rob David <rob.j.david@googlemail.com>
2022-10-24 22:27:35 +0100

    Factor out result as default assumption from iterators

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -4,7 +4,6 @@ package iterator
 import (
 	"fmt"
 
-	"github.com/robdavid/genutil-go/errors/handler"
 	"github.com/robdavid/genutil-go/errors/result"
 	"github.com/robdavid/genutil-go/option"
 )
@@ -14,16 +13,14 @@ type Iterator[T any] interface {
 	// Set the iterator's current value to be the first, and subsequent, iterator elements.
 	// False is returned when there are no more elements (the current value remains unchanged)
 	Next() bool
-	Value() (T, error)             // Get the current iterator value, or an error if the last call to Next() has failed
-	Must() T                       // Get the current iterator value. Panics if the last call to Next() has failed.
-	Try() T                        // Get the current iterator value. Creates a Try() panic if the the value has an error.
-	Abort()                        // Stop the iterator; subsequent calls to Next() will return false.
-	Chan() <-chan result.Result[T] // Return iterator as a channel
+	Value() T       // Get the current iterator value, or an error if the last call to Next() has failed
+	Abort()         // Stop the iterator; subsequent calls to Next() will return false.
+	Chan() <-chan T // Return iterator as a channel
 }
 
 // A function supporting a transforming operation by consuming
 // all or part of an iterator, returning the next value
-type FuncNext[T any, U any] func(Iterator[T]) (bool, U, error)
+type FuncNext[T any, U any] func(Iterator[T]) (bool, U)
 
 func safeClose[T any](ch chan T) (ok bool) {
 	defer func() {
@@ -46,12 +43,12 @@ func safeSend[T any](ch chan<- T, val T) (ok bool) {
 }
 
 // Generic channel implementation
-func iterChan[T any](iter Iterator[T]) (out chan result.Result[T]) {
-	out = make(chan result.Result[T])
+func iterChan[T any](iter Iterator[T]) (out chan T) {
+	out = make(chan T)
 	go func() {
 		defer safeClose(out)
 		for iter.Next() {
-			if !safeSend(out, result.From(iter.Value())) {
+			if !safeSend(out, iter.Value()) {
 				break
 			}
 		}
@@ -63,31 +60,23 @@ func iterChan[T any](iter Iterator[T]) (out chan result.Result[T]) {
 type Func[T, U any] struct {
 	base    Iterator[T]
 	mapping FuncNext[T, U]
-	value   option.Option[result.Result[U]]
-	outChan chan result.Result[U]
+	value   option.Option[U]
+	outChan chan U
 }
 
 func (i *Func[T, U]) Next() bool {
-	ok, value, err := i.mapping(i.base)
+	ok, value := i.mapping(i.base)
 	if !ok {
 		i.value.Clear()
 		return false
 	} else {
-		i.value.Set(result.From(value, err))
+		i.value.Set(value)
 		return true
 	}
 }
 
-func (i *Func[T, U]) Value() (u U, err error) {
-	return i.value.GetOrZero().ToRef().Return()
-}
-
-func (i *Func[T, U]) Must() U {
-	return i.value.GetOrZero().ToRef().Must()
-}
-
-func (i *Func[T, U]) Try() U {
-	return i.value.GetOrZero().ToRef().Try()
+func (i *Func[T, U]) Value() U {
+	return i.value.GetOrZero()
 }
 
 func (i *Func[T, U]) Abort() {
@@ -98,7 +87,7 @@ func (i *Func[T, U]) Abort() {
 	i.base.Abort()
 }
 
-func (i *Func[T, U]) Chan() <-chan result.Result[U] {
+func (i *Func[T, U]) Chan() <-chan U {
 	if i.outChan == nil {
 		i.outChan = iterChan[U](i)
 	}
@@ -106,7 +95,7 @@ func (i *Func[T, U]) Chan() <-chan result.Result[U] {
 }
 
 func WrapFunc[T any, U any](iterator Iterator[T], f FuncNext[T, U]) Iterator[U] {
-	return &Func[T, U]{base: iterator, mapping: f, value: option.Empty[result.Result[U]]()}
+	return &Func[T, U]{base: iterator, mapping: f, value: option.Empty[U]()}
 }
 
 // Iterator over a slice
@@ -114,7 +103,7 @@ type SliceIter[T any] struct {
 	slice   []T
 	index   int
 	value   T
-	outChan chan result.Result[T]
+	outChan chan T
 }
 
 func (si *SliceIter[T]) Next() bool {
@@ -127,16 +116,8 @@ func (si *SliceIter[T]) Next() bool {
 	}
 }
 
-func (si *SliceIter[T]) Must() T {
+func (si *SliceIter[T]) Value() T {
 	return si.value
-}
-
-func (si *SliceIter[T]) Try() T {
-	return si.value
-}
-
-func (si *SliceIter[T]) Value() (T, error) {
-	return si.value, nil
 }
 
 func (si *SliceIter[T]) Abort() {
@@ -146,7 +127,7 @@ func (si *SliceIter[T]) Abort() {
 	}
 }
 
-func (si *SliceIter[T]) Chan() <-chan result.Result[T] {
+func (si *SliceIter[T]) Chan() <-chan T {
 	if si.outChan == nil {
 		si.outChan = iterChan[T](si)
 	}
@@ -165,62 +146,51 @@ func Of[T any](elements ...T) Iterator[T] {
 	return &SliceIter[T]{elements, 0, t, nil}
 }
 
-func wrapMap[T any, U any](mapping func(T) (U, error)) FuncNext[T, U] {
-	return func(iterator Iterator[T]) (ok bool, value U, err error) {
+func wrapMap[T any, U any](mapping func(T) U) FuncNext[T, U] {
+	return func(iterator Iterator[T]) (ok bool, value U) {
 		if ok = iterator.Next(); ok {
-			var vt T
-			if vt, err = iterator.Value(); err == nil {
-				value, err = mapping(vt)
-			}
+			value = mapping(iterator.Value())
 		}
 		return
 	}
 }
 
-// Wraps an iterator with a mapping function that may return an error, producing a new iterator
-func DoMap[T any, U any](iter Iterator[T], mapping func(T) (U, error)) Iterator[U] {
-	return WrapFunc[T, U](iter, wrapMap(mapping))
-}
-
 // Wraps an iterator with a mapping function, producing a new iterator
 func Map[T any, U any](iter Iterator[T], mapping func(T) U) Iterator[U] {
-	action := func(t T) (U, error) {
-		return mapping(t), nil
-	}
-	return WrapFunc[T, U](iter, wrapMap(action))
+	action := wrapMap(func(t T) U { return mapping(t) })
+	return WrapFunc(iter, action)
 }
 
-// Collects all elements from an iterator into a slice. If the iterator
-// returns an error, this call will panic.
-func MustCollect[T any](iter Iterator[T]) []T {
-	return handler.Must(Collect(iter))
-}
-
-// Collects all elements from an iterator into a slice. If the iterator
-// returns an error, this call will create handler.Try type panic.
-func TryCollect[T any](iter Iterator[T]) []T {
-	return handler.Try(Collect(iter))
-}
-
-// Collects all elements from an iterator into a slice. If the iterator
-// returns an error, this call will terminate and return that error.
-func Collect[T any](iter Iterator[T]) ([]T, error) {
+// Collects all elements from an iterator into a slice.
+func Collect[T any](iter Iterator[T]) []T {
 	result := make([]T, 0)
 	for iter.Next() {
-		if val, err := iter.Value(); err != nil {
-			return result, err
-		} else {
-			result = append(result, val)
-		}
+		result = append(result, iter.Value())
 	}
-	return result, nil
+	return result
 }
 
-// A generator pattern for an iterator, using
-// a go routine and channel
+// Collects all elements from an iterator of results into a result of slice of the iterator's underlying type
+// If the iterator returns an error result at any point, this call will terminate and return that error in the
+// result, along with the elements collected thus far.
+func CollectResults[T any](iter Iterator[result.Result[T]]) result.Result[[]T] {
+	collectResult := make([]T, 0)
+	for iter.Next() {
+		res := iter.Value()
+		if res.IsError() {
+			iter.Abort()
+			return result.From(collectResult, res.GetErr())
+		}
+		collectResult = append(collectResult, res.Get())
+	}
+	return result.Value(collectResult)
+}
+
+// An iterator that obtains values (or an error) from
+// a channel
 type GenIter[T any] struct {
-	source chan result.Result[T]
-	value  result.Result[T]
+	source chan T
+	value  T
 }
 
 func (pi *GenIter[T]) Next() bool {
@@ -229,19 +199,11 @@ func (pi *GenIter[T]) Next() bool {
 	return ok
 }
 
-func (pi *GenIter[T]) Value() (T, error) {
-	return pi.value.Return()
+func (pi *GenIter[T]) Value() T {
+	return pi.value
 }
 
-func (pi *GenIter[T]) Must() T {
-	return pi.value.Must()
-}
-
-func (pi *GenIter[T]) Try() T {
-	return pi.value.Must()
-}
-
-func (pi *GenIter[T]) Chan() <-chan result.Result[T] {
+func (pi *GenIter[T]) Chan() <-chan T {
 	return pi.source
 }
 
@@ -254,17 +216,13 @@ type AbortGenerator struct{}
 // An object to which consecutive iterator values are supplied via the Yield
 // method (or an error via the Error method), sent via a channel
 type Yield[T any] struct {
-	sink chan result.Result[T]
+	sink chan T
 }
 
 func (y Yield[T]) Yield(t T) {
-	if !safeSend(y.sink, result.Value(t)) {
+	if !safeSend(y.sink, t) {
 		panic(AbortGenerator{})
 	}
-}
-
-func (y Yield[T]) Error(err error) {
-	safeSend(y.sink, result.Error[T](err))
 }
 
 // An error type indicating that a Pipe iterator function has panicked
@@ -278,32 +236,29 @@ func (pp GeneratorPanic) Error() string {
 
 // A type alias for a function taking a Yield object and returning
 // an error.
-type GenFunc[T any] func(Yield[T]) error
+type GenFunc[T any] func(Yield[T])
 
 func runGenerator[T any](y Yield[T], activity GenFunc[T]) {
 	defer safeClose(y.sink)
 	defer func() {
 		if p := recover(); p != nil {
 			if _, abort := p.(AbortGenerator); !abort {
-				y.Error(GeneratorPanic{p})
+				panic(p)
 			}
 		}
 	}()
-	if err := activity(y); err != nil {
-		y.Error(err)
-	}
+	activity(y)
 }
 
 // Create an iterator from a function, passed in activity, that yields a sequence of results
-// by making calls to Yield(), or Error(), on an object that will be passed to the function.
+// by making calls to Yield(), on an object that will be passed to the function.
 // The function is run in a separate goroutine, and its yielded results are sent over a channel
 // to the iterator where they can be consumed in the normal way by calls to Next() and Value().
-// Any error result yielded, or returned by the function, cause that error to be returned by Value()
-// in the iterator.
-//
-// The activity parameter is of type PipeFunc[T], which is an alias for func(Yield[T]) error
+// The channel itself is available via the Chan() method. A call to Abort() will cause the channel
+// to close and no further elements will be produced by Next() or a read of the channel.
+// The activity parameter is of type PipeFunc[T], which is an alias for func(Yield[T])
 func Generate[T any](activity GenFunc[T]) Iterator[T] {
-	ch := make(chan result.Result[T])
+	ch := make(chan T)
 	yield := Yield[T]{ch}
 	go runGenerator(yield, activity)
 	return &GenIter[T]{source: ch}
--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/robdavid/genutil-go/errors/result"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSliceIter(t *testing.T) {
 	input := []int{1, 2, 3, 4}
 	iter := Slice(input)
-	output := MustCollect(iter)
+	output := Collect(iter)
 	assert.Equal(t, input, output)
 }
 
@@ -19,7 +20,7 @@ func TestSliceIterChan(t *testing.T) {
 	iter := Slice(input)
 	i := 1
 	for v := range iter.Chan() {
-		assert.Equal(t, v.Get(), i)
+		assert.Equal(t, v, i)
 		i++
 	}
 }
@@ -29,7 +30,7 @@ func TestSliceIterChanAbort(t *testing.T) {
 	iter := Slice(input)
 	i := 1
 	for v := range iter.Chan() {
-		assert.Equal(t, v.Get(), i)
+		assert.Equal(t, v, i)
 		i++
 		if i == 3 {
 			iter.Abort()
@@ -41,34 +42,30 @@ func TestSliceIterChanAbort(t *testing.T) {
 func TestMapIter(t *testing.T) {
 	input := []int{1, 2, 3, 4}
 	expected := []int{2, 4, 6, 8}
-	actual := MustCollect(Map(Slice(input), func(n int) int { return n * 2 }))
+	actual := Collect(Map(Slice(input), func(n int) int { return n * 2 }))
 	assert.Equal(t, expected, actual)
 }
 
 func TestDoMapIter(t *testing.T) {
 	input := []int{1, 2, 3, 4}
-	expected := []int{2, 4, 6}
-	actual, err := Collect(DoMap(Slice(input), func(n int) (int, error) {
+	expected := result.From([]int{2, 4, 6}, fmt.Errorf("Value 4 too large"))
+	actual := CollectResults(Map(Slice(input), func(n int) result.Result[int] {
 		if n < 4 {
-			return n * 2, nil
+			return result.Value(n * 2)
 		} else {
-			return 0, fmt.Errorf("Value %d too large", n)
+			return result.Error[int](fmt.Errorf("Value %d too large", n))
 		}
 	}))
 	assert.Equal(t, expected, actual)
-	if assert.Error(t, err) {
-		assert.Equal(t, "Value 4 too large", err.Error())
-	}
 }
 
-func TestGeneratorIter(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
+func TestGenerator(t *testing.T) {
+	gen := Generate(func(y Yield[int]) {
 		for i := 0; i < 10; i++ {
 			y.Yield(i)
 		}
-		return nil
 	})
-	actual := MustCollect(pipe)
+	actual := Collect(gen)
 	expected := make([]int, 10)
 	for i := range expected {
 		expected[i] = i
@@ -76,18 +73,17 @@ func TestGeneratorIter(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestGeneratorIterChan(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
+func TestGeneratorChan(t *testing.T) {
+	gen := Generate(func(y Yield[int]) {
 		for i := 0; i < 10; i++ {
 			y.Yield(i)
 		}
-		return nil
 	})
 	actual := make([]int, 10)
 	expected := make([]int, 10)
 	p := 0
-	for i := range pipe.Chan() {
-		actual[p] = i.Get()
+	for i := range gen.Chan() {
+		actual[p] = i
 		p++
 	}
 	for i := range expected {
@@ -96,22 +92,20 @@ func TestGeneratorIterChan(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestGeneratorIterChanAbort(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
+func TestGeneratorChanAbort(t *testing.T) {
+	gen := Generate(func(y Yield[int]) {
 		for i := 0; i < 10; i++ {
 			y.Yield(i)
 		}
-		return nil
 	})
 	actual := make([]int, 5)
 	expected := make([]int, 5)
 	p := 0
-	for i := range pipe.Chan() {
-		assert.False(t, i.IsError())
-		actual[p] = i.Get()
+	for i := range gen.Chan() {
+		actual[p] = i
 		p++
 		if p >= len(actual) {
-			pipe.Abort()
+			gen.Abort()
 		}
 	}
 	for i := range expected {
@@ -120,14 +114,35 @@ func TestGeneratorIterChanAbort(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestGeneratorIterMap(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
+func TestGeneratorIterAbort(t *testing.T) {
+	gen := Generate(func(y Yield[int]) {
 		for i := 0; i < 10; i++ {
 			y.Yield(i)
 		}
-		return nil
 	})
-	actual := MustCollect(Map(pipe, func(x int) int { return x * 3 }))
+	actual := make([]int, 5)
+	expected := make([]int, 5)
+	p := 0
+	for gen.Next() {
+		actual[p] = gen.Value()
+		p++
+		if p >= len(actual) {
+			gen.Abort()
+		}
+	}
+	for i := range expected {
+		expected[i] = i
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestGeneratorMap(t *testing.T) {
+	gen := Generate(func(y Yield[int]) {
+		for i := 0; i < 10; i++ {
+			y.Yield(i)
+		}
+	})
+	actual := Collect(Map(gen, func(x int) int { return x * 3 }))
 	expected := make([]int, 10)
 	for i := range expected {
 		expected[i] = i * 3
@@ -135,38 +150,18 @@ func TestGeneratorIterMap(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestGeneratorIterError(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
+func TestGeneratorError(t *testing.T) {
+	gen := Generate(func(y Yield[result.Result[int]]) {
 		for i := 0; i < 10; i++ {
-			y.Yield(i)
+			y.Yield(result.Value(i))
 		}
-		return fmt.Errorf("iterator failed")
+		y.Yield(result.Error[int](fmt.Errorf("iterator failed")))
 	})
-	actual, err := Collect(pipe)
+	actual := CollectResults(gen)
 	expected := make([]int, 10)
 	for i := range expected {
 		expected[i] = i
 	}
-	assert.Equal(t, expected, actual)
-	if assert.Error(t, err) {
-		assert.Equal(t, "iterator failed", err.Error())
-	}
-}
-
-func TestGeneratorIterPanic(t *testing.T) {
-	pipe := Generate(func(y Yield[int]) error {
-		for i := 0; i < 10; i++ {
-			y.Yield(i)
-		}
-		panic("iterator failed")
-	})
-	actual, err := Collect(pipe)
-	expected := make([]int, 10)
-	for i := range expected {
-		expected[i] = i
-	}
-	assert.Equal(t, expected, actual)
-	if assert.Error(t, err) {
-		assert.Equal(t, GeneratorPanic{"iterator failed"}, err)
-	}
+	assert.Equal(t, expected, actual.Get())
+	assert.EqualError(t, actual.GetErr(), "iterator failed")
 }
