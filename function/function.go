023bce0c90fde3d2bec19093dd8c97fe5c0ca816
Rob David <rob.j.david@googlemail.com>
2022-10-30 16:13:49 +0000

    Start function package

---
+++ function/function.go
@@ -0,0 +1,26 @@
+package function
+
+// Identity function - returns the value passed
+func Id[T any](v T) T {
+	return v
+}
+
+// Ternary logic function. If `cond` is true, `v` is returned.
+// Otherwise `alt` is returned.
+func IfElse[T any](cond bool, v T, alt T) T {
+	if cond {
+		return v
+	} else {
+		return alt
+	}
+}
+
+// Ternary logic function. If `cond` is true, the value obtained by
+// evaluating `f` is returned. Otherwise `alt` is evaluated and returned.
+func IfElseF[T any](cond bool, f func() T, alt func() T) T {
+	if cond {
+		return f()
+	} else {
+		return alt()
+	}
+}
---
+++ function/function_test.go
@@ -0,0 +1,27 @@
+package function
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfElse(t *testing.T) {
+	const yes = "yes"
+	const no = "no"
+	assert.Equal(t, IfElse(true, yes, no), "yes")
+	assert.Equal(t, IfElse(false, yes, no), "no")
+}
+
+func TestIfElseF(t *testing.T) {
+	const yes = "yes"
+	const no = "no"
+	assert.Equal(t, IfElseF(true, func() string { return yes }, func() string { return no }), "yes")
+	assert.Equal(t, IfElseF(false, func() string { return yes }, func() string { return no }), "no")
+}
+
+func TestId(t *testing.T) {
+	assert.Equal(t, 6, Id(6))
+	assert.Equal(t, 12.3, Id(12.3))
+	assert.Equal(t, "hello", Id("hello"))
+}
