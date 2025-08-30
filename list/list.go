0f64ebe00ddf0960f6b2d7742aca664e6ca43ec3
Rob David <rob.j.david@googlemail.com>
2025-08-30 05:54:44 +0000

    Improvements to list including code comments

--- list/list.go
+++ list/list.go
@@ -8,20 +8,25 @@ import (
 )
 
 type Node[T any] struct {
-	Value T
-	Prev  *Node[T]
-	Next  *Node[T]
+	value T
+	prev  *Node[T]
+	next  *Node[T]
 }
 
+// List is a doubly linked list. More precisely it references an item within such a list, which
+// may be at the beginning, the end, or any point in between. The list can be traversed in either
+// direction. The list may be empty, in which case it references nothing.
 type List[T any] struct {
 	head *Node[T]
 }
 
+// New creates a new empty list
 func New[T any]() List[T] {
 	return List[T]{nil}
 }
 
-func From[T any](slice ...T) List[T] {
+// Of creates a new list whose elements are taken from the variadic args of the function
+func Of[T any](slice ...T) List[T] {
 	lst := New[T]()
 	l := len(slice)
 	for i := l - 1; i >= 0; i-- {
@@ -30,78 +35,152 @@ func From[T any](slice ...T) List[T] {
 	return lst
 }
 
+// From creates a new list whose elements are taken from the supplied iterator.
+func From[T any](itr iterator.Iterator[T]) List[T] {
+	return FromSeq(itr.Seq())
+}
+
+// FromSeq creates a new list whose elements are taken from the supplied iter.Seq iterator.
+func FromSeq[T any](itr iter.Seq[T]) List[T] {
+	last := New[T]()
+	var first List[T]
+	for e := range itr {
+		last.Append(e)
+		if n := last.Next(); n.head != nil {
+			last = n
+		} else {
+			first = last
+		}
+	}
+	return first
+}
+
+// IsEmpty returns true if the list is empty
+func (l List[T]) IsEmpty() bool {
+	return l.head == nil
+}
+
+// Get returns the element at the head of the given list. If the list is empty, an empty
+// option is returned; otherwise it will hold the element's value.
 func (l List[T]) Get() option.Option[T] {
 	if l.head == nil {
 		return option.Empty[T]()
 	} else {
-		return option.Value(l.head.Value)
+		return option.Value(l.head.value)
 	}
 }
 
+// Ref returns a reference to the element at the head of the given list. It returns nil if
+// the list is empty.
 func (l List[T]) Ref() *T {
 	if l.head == nil {
 		return nil
 	} else {
-		return &l.head.Value
+		return &l.head.value
 	}
 }
 
+// Set sets the value at the head of the given list. If the list is empty, no action is taken
+// and false is returned. Otherwise the element value is set and true is returned.
 func (l List[T]) Set(value T) bool {
 	if l.head == nil {
 		return false
 	} else {
-		l.head.Value = value
+		l.head.value = value
 		return true
 	}
 }
 
-func (l List[T]) Next() option.Option[List[T]] {
-	if l.head == nil || l.head.Next == nil {
-		return option.Empty[List[T]]()
-	} else {
-		return option.Value(List[T]{l.head.Next})
+// Next returns the list following the head element. If there are not elements following, or if
+// the list is empty, an empty list is returned.
+func (l List[T]) Next() List[T] {
+	head := l.head
+	if head != nil {
+		head = head.next
 	}
+	return List[T]{head}
 }
 
-func (l List[T]) Prev() option.Option[List[T]] {
-	if l.head == nil || l.head.Prev == nil {
-		return option.Empty[List[T]]()
-	} else {
-		return option.Value(List[T]{l.head.Prev})
+// Prev returns the list prior to the head element. If there is no element prior, or if the list is
+// empty, an empty list is returned.
+func (l List[T]) Prev() List[T] {
+	head := l.head
+	if head != nil {
+		head = head.prev
 	}
+	return List[T]{head}
 }
 
+// First returns the list starting at the earliest element in the list, found by iterating backwards.
+// If the list is empty, an empty list is returned.
+func (l List[T]) First() List[T] {
+	var first *Node[T]
+	for n := l.head; n != nil; n = n.prev {
+		first = n
+	}
+	return List[T]{first}
+}
+
+// Last returns the list starting at the final element in the list, found by iterating backwards.
+// If the list is empty, an empty list is returned.
+func (l List[T]) Last() List[T] {
+	var last *Node[T]
+	for n := l.head; n != nil; n = n.next {
+		last = n
+	}
+	return List[T]{last}
+}
+
+// Size returns the number of elements in the list counting from the current element and moving
+// forward.
 func (l List[T]) Size() int {
 	total := 0
-	for n := l.head; n != nil; n = n.Next {
+	for n := l.head; n != nil; n = n.next {
 		total++
 	}
 	return total
 }
 
+// RevSize returns the number of elements in the list, counting from the current element and
+// moving backward.
+func (l List[T]) RevSize() int {
+	total := 0
+	for n := l.head; n != nil; n = n.prev {
+		total++
+	}
+	return total
+}
+
+// Insert inserts a new element in the current position, moving the original element to following
+// position.
 func (l *List[T]) Insert(value T) {
 	if l.head == nil {
-		l.head = &Node[T]{Value: value}
+		l.head = &Node[T]{value: value}
 	} else {
-		l.head = &Node[T]{Value: value, Next: l.head, Prev: l.head.Prev}
+		node := &Node[T]{value: value, next: l.head, prev: l.head.prev}
+		l.head.prev = node
+		l.head = node
 	}
 }
 
+// Append adds an element to the end of the list
 func (l *List[T]) Append(value T) {
 	var node *Node[T]
-	for node = l.head; node != nil && node.Next != nil; node = node.Next {
+	for node = l.head; node != nil && node.next != nil; node = node.next {
 	}
-	newNode := Node[T]{Value: value, Prev: node}
+	newNode := Node[T]{value: value, prev: node}
 	if node == nil {
 		l.head = &newNode
 	} else {
-		node.Next = &newNode
+		node.next = &newNode
 	}
 }
 
+// SeqList returns a native iter.Seq iterator over items moving forward in the list, returning
+// a new List starting at each element found.
 func (l List[T]) SeqList() iter.Seq[List[T]] {
 	return func(yield func(List[T]) bool) {
-		for n := l.head; n != nil; n = n.Next {
+		for n := l.head; n != nil; n = n.next {
 			if !yield(List[T]{n}) {
 				break
 			}
@@ -109,22 +188,62 @@ func (l List[T]) SeqList() iter.Seq[List[T]] {
 	}
 }
 
-func (l List[T]) Seq() iter.Seq[T] {
-	return func(yield func(T) bool) {
-		for n := l.head; n != nil; n = n.Next {
-			if !yield(n.Value) {
+// RevSeqList returns a native iter.Seq iterator over elements moving backwards in the list, returning
+// a new List starting at each element found.
+func (l List[T]) RevSeqList() iter.Seq[List[T]] {
+	return func(yield func(List[T]) bool) {
+		for n := l.head; n != nil; n = n.prev {
+			if !yield(List[T]{n}) {
 				break
 			}
 		}
 	}
 }
 
+// Seq returns a native iter.Seq iterator over element values moving forwards in the list.
+func (l List[T]) Seq() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for n := l.head; n != nil; n = n.next {
+			if !yield(n.value) {
+				break
+			}
+		}
+	}
+}
+
+// RevSeq returns a native iter.Seq iterator over element values moving backwards in the list.
+func (l List[T]) RevSeq() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for n := l.head; n != nil; n = n.prev {
+			if !yield(n.value) {
+				break
+			}
+		}
+	}
+}
+
+// IterList returns a iterator.Iterator over items moving forward in the list, returning
+// a new List starting at each element found.
 func (l List[T]) IterList() iterator.Iterator[List[T]] {
 	return iterator.NewWithSize(l.SeqList(),
 		func() iterator.IteratorSize { return iterator.NewSize(l.Size()) })
 }
 
+// RevIterList returns a iterator.Iterator over items moving forward in the list, returning
+// a new List starting at each element found.
+func (l List[T]) RevIterList() iterator.Iterator[List[T]] {
+	return iterator.NewWithSize(l.RevSeqList(),
+		func() iterator.IteratorSize { return iterator.NewSize(l.RevSize()) })
+}
+
+// Iter returns an iterator.Iterator over element values, moving forwards in the list.
 func (l List[T]) Iter() iterator.Iterator[T] {
 	return iterator.NewWithSize(l.Seq(),
 		func() iterator.IteratorSize { return iterator.NewSize(l.Size()) })
+}
+
+// Iter returns an iterator.Iterator over element values, moving backwards in the list.
+func (l List[T]) RevIter() iterator.Iterator[T] {
+	return iterator.NewWithSize(l.RevSeq(),
+		func() iterator.IteratorSize { return iterator.NewSize(l.RevSize()) })
 }
--- list/list_test.go
+++ list/list_test.go
@@ -14,6 +14,11 @@ import (
 func TestEmpty(t *testing.T) {
 	empty := New[int]()
 	assert.Equal(t, 0, empty.Size())
+	assert.True(t, empty.IsEmpty())
+	assert.True(t, empty.Prev().IsEmpty())
+	assert.True(t, empty.Next().IsEmpty())
+	assert.True(t, empty.First().IsEmpty())
+	assert.True(t, empty.Last().IsEmpty())
 	assert.True(t, empty.Get().ToRef().IsEmpty())
 }
 
@@ -26,13 +31,9 @@ func TestAppend(t *testing.T) {
 	assert.Equal(t, size, list.Size())
 	l := list
 	for i := range size {
-		var ok bool
 		assert.Equal(t, option.Value(i), l.Get())
-		l, ok = l.Next().GetOK()
-		require.Equal(t, i != size-1, ok)
-		if ok {
-			require.NotNil(t, l.head)
-		}
+		l = l.Next()
+		require.Equal(t, i == size-1, l.IsEmpty())
 	}
 }
 
@@ -48,12 +49,18 @@ func TestInsert(t *testing.T) {
 		n, ok := l.Get().GetOK()
 		require.True(t, ok)
 		assert.Equal(t, size-1-i, n)
-		l, ok = l.Next().GetOK()
-		require.Equal(t, i != size-1, ok)
-		if ok {
-			require.NotNil(t, l.head)
-		}
+		l = l.Next()
+		require.Equal(t, i == size-1, l.IsEmpty())
 	}
+}
+
+func TestLastAndFirst(t *testing.T) {
+	const size = 10
+	list := Of(slices.Range(1, size+1)...)
+	last := list.Last()
+	assert.Equal(t, option.Value(size), last.Get())
+	assert.Equal(t, size, last.RevSize())
+	assert.Equal(t, option.Value(1), last.First().Get())
 }
 
 func TestIterator(t *testing.T) {
@@ -68,6 +75,34 @@ func TestIterator(t *testing.T) {
 	assert.Equal(t, size, cap(collected))
 }
 
+func TestRevIter(t *testing.T) {
+	const size = 10
+	list := From(iterator.Range(0, size))
+	last := list.Last()
+	reved := last.RevIter().Collect()
+	assert.Equal(t, slices.IncRange(size-1, 0), reved)
+}
+
+func TestRevIterList(t *testing.T) {
+	const size = 10
+	defer test.ReportErr(t)
+	list := From(iterator.Range(0, size))
+	last := list.Last()
+	revedList := last.RevIterList().Collect()
+	reved := slices.Map(revedList, func(l List[int]) int { return l.Get().Try() })
+	assert.Equal(t, slices.IncRange(size-1, 0), reved)
+}
+
+func TestRef(t *testing.T) {
+	const size = 10
+	list := From(iterator.Range(0, size))
+	for itr := range list.SeqList() {
+		*itr.Ref()++
+	}
+	c := list.Iter().Collect()
+	assert.Equal(t, slices.Range(1, size+1), c)
+}
+
 func TestListIterator(t *testing.T) {
 	defer test.ReportErr(t)
 	const size = 10
@@ -79,8 +114,8 @@ func TestListIterator(t *testing.T) {
 	for i, l := range list.IterList().Enumerate().Seq2() {
 		if i > 0 && i < size-1 {
 			v := l.Get().Try()
-			p := l.Prev().Try().Get().Try()
-			n := l.Next().Try().Get().Try()
+			p := l.Prev().Get().Try()
+			n := l.Next().Get().Try()
 			assert.Equal(t, i, v)
 			assert.Equal(t, i-1, p)
 			assert.Equal(t, i+1, n)
@@ -113,6 +148,6 @@ func TestListSet(t *testing.T) {
 func TestFrom(t *testing.T) {
 	const size = 10
 	input := slices.Range(0, size)
-	lst := From(input...)
+	lst := Of(input...)
 	assert.Equal(t, input, lst.Iter().Collect())
 }
